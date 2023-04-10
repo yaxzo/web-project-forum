9db7bba7937d6c2aa354fb82527cae2e895848d6
yaxzo <100714739+yaxzo@users.noreply.github.com>
2023-04-10 22:57:22 +0300

    [ADD] file field in trad_form.py

--- project/forms/trad_form.py
+++ project/forms/trad_form.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
 
 from wtforms.fields import StringField, TextAreaField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
@@ -6,7 +7,8 @@ from wtforms.validators import DataRequired
 
 class CreateTradForm(FlaskForm):
     title = StringField("Заголовок", validators=[DataRequired()])
-    preview = StringField("Превью", validators=[DataRequired()])
+    preview = StringField("Превью (макс. 70 слов)", validators=[DataRequired()])
     content = TextAreaField("Содержание статьи", validators=[DataRequired()])
+    photo = FileField("Обложка обсуждения (необязательно)")
     is_private = BooleanField("Сделать приватным?")
     submit = SubmitField("Создать")

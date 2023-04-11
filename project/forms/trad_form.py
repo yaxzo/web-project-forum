0ae594e0d78b19d34a45f4a07956e0e79c7fe56d
yaxzo <100714739+yaxzo@users.noreply.github.com>
2023-04-11 18:47:30 +0300

    [FIX] trad <-> articles

--- project/forms/trad_form.py
+++ project/forms/trad_form.py
@@ -8,7 +8,7 @@ from wtforms.validators import DataRequired
 class CreateTradForm(FlaskForm):
     title = StringField("Заголовок", validators=[DataRequired()])
     preview = StringField("Превью (макс. 70 слов)", validators=[DataRequired()])
-    content = TextAreaField("Содержание статьи", validators=[DataRequired()])
+    content = TextAreaField("Содержание обсуждения", validators=[DataRequired()])
     photo = FileField("Обложка обсуждения (необязательно)")
     is_private = BooleanField("Сделать приватным?")
     submit = SubmitField("Создать")

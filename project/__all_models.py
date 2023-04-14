db99e4a4eb28539fb359b79c393d04a1f4487881
yaxzo <100714739+yaxzo@users.noreply.github.com>
2023-04-14 22:09:02 +0300

    [ADD] new data files

---
+++ project/__all_models.py
@@ -0,0 +1,3 @@
+from . import user
+from . import trad
+from . import trad_comments
---
+++ project/trad.py
@@ -0,0 +1,33 @@
+import datetime
+
+from flask_login import UserMixin
+
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from data.db_session import SqlAlchemyBase
+
+
+class Trad(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = "trads"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True,
+                           autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String,
+                              nullable=False)
+    preview = sqlalchemy.Column(sqlalchemy.String,
+                                nullable=False)
+    content = sqlalchemy.Column(sqlalchemy.String,
+                                nullable=False)
+    author_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                  sqlalchemy.ForeignKey("users.id"))
+    trad_photo = sqlalchemy.Column(sqlalchemy.String,
+                                   default="default.webp",
+                                   nullable=False)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.date.today())
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean,
+                                   nullable=False)
+    user = orm.relationship("User")

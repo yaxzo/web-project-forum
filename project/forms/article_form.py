0fcf237355bc4b79cc919e79a4567e3a5fbaea23
yaxzo <100714739+yaxzo@users.noreply.github.com>
2023-04-18 17:51:35 +0300

    [ADD] new forms

---
+++ project/forms/article_form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+
+from wtforms.fields import StringField, TextAreaField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class CreateArticleForm(FlaskForm):
+    title = StringField("Заголовок", validators=[DataRequired()])
+    preview = StringField("Превью (макс. 70 слов)", validators=[DataRequired()])
+    content = TextAreaField("Содержание статьи", validators=[DataRequired()])
+    photo = FileField("Обложка статьи (необязательно)")
+    is_private = BooleanField("Сделать приватным?")
+    submit = SubmitField("Создать")
+
